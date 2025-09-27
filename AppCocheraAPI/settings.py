4ce3a1a8baf48db74029ee668b8a963f3d94276d
Superalst <agustinrojas99@hotmail.com>
2025-09-27 18:15:11 -0300

    Subiendo a web

--- AppCocheraAPI/settings.py
+++ AppCocheraAPI/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x5jq@+69&j-ml9=+brrh@u&oc)zo=)h7m45^e*v5s9v3(04pn_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://cuberpa.it.com']
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Application definition
 
 INSTALLED_APPS = [
